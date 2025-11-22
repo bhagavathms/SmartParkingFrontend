5db0cd1e62341b659222c80ff4beb89bd68689d4
Bhagavath M S <30797946+bhagavathms@users.noreply.github.com>
2025-11-22 19:55:17 +0530

    Refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-
-// Mount the main App component into the root div (index.html)
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
 root.render(<App />);
